7577894815f285b96791d84603f28a8a58ebb8ec
ansango <anibalsantosgo@gmail.com>
2022-06-19 18:47:51 +0200

    data

---
+++ lib/data/index.ts
@@ -0,0 +1,96 @@
+export const personal = {
+  name: "Aníbal Santos Gómez",
+  title: "Frontend Mid Developer",
+  email: "anibalsantosgo@gmail.com",
+  linkedin: "https://www.linkedin.com/in/ansango/",
+  github: "https://github.com/ansango",
+};
+
+export const skills = [
+  {
+    type: "Frontend",
+    skills: [
+      "HTML5",
+      "CSS3",
+      "JavaScript ES6 >+",
+      "jQuery",
+      "TypeScript",
+      "React",
+      "Next.js",
+      "Angular",
+      "Vue",
+      "Nuxt",
+      "Svelte",
+      "Redux",
+      "Redux Toolkit",
+      "Sass",
+      "Less",
+      "Tailwindcss",
+      "Bootstrap",
+      "Bulma",
+    ],
+  },
+  {
+    type: "Backend",
+    skills: [
+      "Node.js",
+      "Express",
+      "Laravel",
+      "Mongoose",
+      "Socket.io",
+      "Server Sent Events",
+      "Passport",
+      "JWT",
+    ],
+  },
+  {
+    type: "Database",
+    skills: ["SQL", "MongoDB"],
+  },
+  {
+    type: "Testing",
+    skills: ["Jest", "Enzyme", "Mocha", "Storybook", "Cypress"],
+  },
+  {
+    type: "Devops",
+    skills: [
+      "GitHub",
+      "GitLab",
+      "Bitbucket",
+      "Vercel",
+      "Heroku",
+      "Netlify",
+      "Atlas MongoDB",
+      "Cloudinary",
+    ],
+  },
+  {
+    type: "Tooling",
+    skills: [
+      "Git",
+      "Git Flow",
+      "Prettier",
+      "ESlint",
+      "Stylelint",
+      "Grunt",
+      "Parcel",
+      "Webpack",
+      "Vite",
+      "Rollup",
+      "SWC",
+      "PostCSS",
+    ],
+  },
+  {
+    type: "UX/UI",
+    skills: ["Sketch", "Figma"],
+  },
+  {
+    type: "Mobile",
+    skills: ["React Native", "Flutter"],
+  },
+  {
+    type: "Other",
+    skills: ["Notion"],
+  },
+];
